7c25c6e33105271c3796d9fb5e20ce1c11c0f6ed
Qonaah Nurussalamah <qnurussalamah@gmail.com>
2023-12-24 17:32:20 +0700

    feat(activities): create model relation to user model

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Activity extends Model
 {
@@ -12,4 +13,8 @@ class Activity extends Model
     protected $keyType = "int";
     protected $timestamps = true;
     protected $incrementing = true;
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(Activity::class, "user_id", "id");
+    }
 }
